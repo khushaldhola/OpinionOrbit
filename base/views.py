0273cc00872931a4635a5ae72d1a4af321765310
khushaldhola <khushaldhola9@gmail.com>
2023-09-29 16:48:15 +0530

    templates in app, passing dict

--- base/views.py
+++ base/views.py
@@ -2,8 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': "Let's learn Bharatiya history"},
+    {'id': 2, 'name': "Data Science with sklearn"},
+    {'id': 3, 'name': "why colleges not worth it"},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
