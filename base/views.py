73b57f00494a552324256210b799890a82381e6c
khushaldhola <khushaldhola9@gmail.com>
2023-09-29 16:08:52 +0530

    Configed templates

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('ROOM Page')
+    return render(request, 'room.html')
